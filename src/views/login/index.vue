3ac4ab87d4bbeec6a7ff1e92a98e1162af6d4aaf
yuanhao1122 <741821607@qq.com>
2025-05-18 22:28:53 +0800

    feat: 登录-表单校验规则

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -19,10 +19,26 @@ const formData = ref({
   username: 'admin',
   password: '111111',
 })
+// 表单检验对象
+const rules = {
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'change' },
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    { min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'change' },
+  ],
+}
+// 表单对象
+const loginForm = ref()
 // 按钮加载效果
 const loading = ref(false)
 // 登录
 const login = async () => {
+  // 校验表单
+  await loginForm.value?.validate()
+
   // 按钮加载效果
   loading.value = true
   // 登录逻辑
@@ -51,21 +67,27 @@ const login = async () => {
     <el-row>
       <el-col :span="12" :sx="0"></el-col>
       <el-col :span="12" :sx="24">
-        <el-form class="login_form">
+        <el-form class="login_form" :model="formData" :rules="rules" ref="loginForm">
           <h1>Hello</h1>
           <h2>欢迎来到硅谷甄选</h2>
-          <el-form-item>
-            <el-input :prefix-icon="User" type="text" v-model="formData.username"></el-input>
+          <el-form-item class="login_form_item" prop="username">
+            <el-input
+              :prefix-icon="User"
+              type="text"
+              v-model="formData.username"
+              class="login_input"
+            ></el-input>
           </el-form-item>
-          <el-form-item>
+          <el-form-item class="login_form_item" prop="password">
             <el-input
               :prefix-icon="Lock"
               type="password"
               :show-password="true"
               v-model="formData.password"
+              class="login_input"
             ></el-input>
           </el-form-item>
-          <el-form-item>
+          <el-form-item class="login_form_item">
             <el-button :loading="loading" class="login_btn" type="primary" @click="login">
               登录
             </el-button>
@@ -76,6 +98,9 @@ const login = async () => {
   </div>
 </template>
 <style scoped lang="scss">
+* {
+  box-sizing: border-box;
+}
 .login_container {
   width: 100%;
   height: 100vh;
@@ -84,10 +109,17 @@ const login = async () => {
   .login_form {
     position: relative;
     width: 80%;
-    height: 30vh;
+    height: 100%;
     top: 30vh;
     padding: 40px;
     background: url('@/assets/images/login_form.png') no-repeat;
+    background-size: cover;
+    .login_form_item {
+      width: 100%;
+    }
+    .login_input {
+      width: 100%;
+    }
     h1 {
       font-size: 40px;
       color: #fff;
